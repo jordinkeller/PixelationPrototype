4c3f0d2f7e8f7d3e3ae3561e9dc23fd9e8ca087d
Jordin Keller <jordinkeller@gmail.com>
2019-12-30 22:01:10 -0700

    Initial Project Commit

---
+++ PixelationPrototypeWinForms/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace PixelationPrototypeWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_btnChoosePic = new System.Windows.Forms.Button();
+            this.UI_btnPixelate = new System.Windows.Forms.Button();
+            this.UI_PictureViewer = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UI_lblFileLocation = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UI_txtPixelSize = new System.Windows.Forms.TextBox();
+            this.UI_btnReduceColors = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.UI_txtColorReduction = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_PictureViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UI_btnChoosePic
+            // 
+            this.UI_btnChoosePic.Location = new System.Drawing.Point(13, 13);
+            this.UI_btnChoosePic.Name = "UI_btnChoosePic";
+            this.UI_btnChoosePic.Size = new System.Drawing.Size(192, 23);
+            this.UI_btnChoosePic.TabIndex = 0;
+            this.UI_btnChoosePic.Text = "Choose Picture";
+            this.UI_btnChoosePic.UseVisualStyleBackColor = true;
+            this.UI_btnChoosePic.Click += new System.EventHandler(this.UI_btnChoosePic_Click);
+            // 
+            // UI_btnPixelate
+            // 
+            this.UI_btnPixelate.Location = new System.Drawing.Point(15, 100);
+            this.UI_btnPixelate.Name = "UI_btnPixelate";
+            this.UI_btnPixelate.Size = new System.Drawing.Size(190, 23);
+            this.UI_btnPixelate.TabIndex = 1;
+            this.UI_btnPixelate.Text = "Pixelate";
+            this.UI_btnPixelate.UseVisualStyleBackColor = true;
+            this.UI_btnPixelate.Click += new System.EventHandler(this.UI_btnPixelate_Click);
+            // 
+            // UI_PictureViewer
+            // 
+            this.UI_PictureViewer.Location = new System.Drawing.Point(211, 13);
+            this.UI_PictureViewer.Name = "UI_PictureViewer";
+            this.UI_PictureViewer.Size = new System.Drawing.Size(376, 328);
+            this.UI_PictureViewer.TabIndex = 2;
+            this.UI_PictureViewer.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Location:";
+            // 
+            // UI_lblFileLocation
+            // 
+            this.UI_lblFileLocation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UI_lblFileLocation.Location = new System.Drawing.Point(88, 39);
+            this.UI_lblFileLocation.Name = "UI_lblFileLocation";
+            this.UI_lblFileLocation.Size = new System.Drawing.Size(117, 23);
+            this.UI_lblFileLocation.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pixel Size:";
+            // 
+            // UI_txtPixelSize
+            // 
+            this.UI_txtPixelSize.Location = new System.Drawing.Point(88, 74);
+            this.UI_txtPixelSize.Name = "UI_txtPixelSize";
+            this.UI_txtPixelSize.Size = new System.Drawing.Size(117, 20);
+            this.UI_txtPixelSize.TabIndex = 6;
+            this.UI_txtPixelSize.TextChanged += new System.EventHandler(this.UI_txtPixelSize_TextChanged);
+            // 
+            // UI_btnReduceColors
+            // 
+            this.UI_btnReduceColors.Enabled = false;
+            this.UI_btnReduceColors.Location = new System.Drawing.Point(15, 177);
+            this.UI_btnReduceColors.Name = "UI_btnReduceColors";
+            this.UI_btnReduceColors.Size = new System.Drawing.Size(190, 23);
+            this.UI_btnReduceColors.TabIndex = 7;
+            this.UI_btnReduceColors.Text = "Reduce Colors";
+            this.UI_btnReduceColors.UseVisualStyleBackColor = true;
+            this.UI_btnReduceColors.Click += new System.EventHandler(this.UI_btnReduceColors_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Reduced Color Amount:";
+            // 
+            // UI_txtColorReduction
+            // 
+            this.UI_txtColorReduction.Location = new System.Drawing.Point(15, 151);
+            this.UI_txtColorReduction.Name = "UI_txtColorReduction";
+            this.UI_txtColorReduction.Size = new System.Drawing.Size(190, 20);
+            this.UI_txtColorReduction.TabIndex = 10;
+            this.UI_txtColorReduction.TextChanged += new System.EventHandler(this.UI_txtColorReduction_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 353);
+            this.Controls.Add(this.UI_txtColorReduction);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.UI_btnReduceColors);
+            this.Controls.Add(this.UI_txtPixelSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UI_lblFileLocation);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UI_PictureViewer);
+            this.Controls.Add(this.UI_btnPixelate);
+            this.Controls.Add(this.UI_btnChoosePic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UI_PictureViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UI_btnChoosePic;
+        private System.Windows.Forms.Button UI_btnPixelate;
+        private System.Windows.Forms.PictureBox UI_PictureViewer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label UI_lblFileLocation;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UI_txtPixelSize;
+        private System.Windows.Forms.Button UI_btnReduceColors;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox UI_txtColorReduction;
+    }
+}
+
---
+++ PixelationPrototypeWinForms/Form1.cs
@@ -0,0 +1,182 @@
+﻿using AForge.Imaging.ColorReduction;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelationPrototypeWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Bitmap originalImage;
+        public int pixelSize;
+        public int colorReductionAmount = 0;
+        public int width;
+        public int height;
+
+        private void UI_btnChoosePic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "E:\\Downloads";
+            openFileDialog.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    originalImage = new Bitmap(openFileDialog.FileName);
+                    width = originalImage.Width;
+                    height = originalImage.Height;
+
+                    // Adjust form and update UI
+                    UI_PictureViewer.Width = originalImage.Width;
+                    UI_PictureViewer.Height = originalImage.Height;
+                    UI_PictureViewer.Image = originalImage;
+                    UI_lblFileLocation.Text = openFileDialog.FileName;
+                    this.Width = UI_PictureViewer.Width + 95;
+                    this.Height = UI_PictureViewer.Height + 95;
+                    CheckPixelSize(UI_txtPixelSize.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error loading picture.");
+                }
+            }
+        }
+
+        private void UI_btnPixelate_Click(object sender, EventArgs e)
+        {
+            Pixelate((Bitmap)UI_PictureViewer.Image, pixelSize);
+        }
+
+        private void Pixelate(Bitmap originalImage, int pixelSize)
+        {
+            var pixelatedImage = new Bitmap(originalImage);
+
+            var pixelBlocksWidth = width / pixelSize;
+            var pixelBlockHeight = height / pixelSize;
+
+            // the image is divided into blocks, determined by pixelSize
+            // loop through the blocks and change the color
+            for (int x = 0; x < width; x+= pixelSize)
+            {
+                for (int y = 0; y < height; y+= pixelSize)
+                {
+                    PixelateBlock((Bitmap)pixelatedImage, x, y, pixelSize);
+                }
+            }
+
+            UI_PictureViewer.Image = (Bitmap)pixelatedImage;
+        }
+
+        private void PixelateBlock(Bitmap image, int x, int y, int pixelSize)
+        {
+            // get the all of the colors within the block into a list
+            List<Color> colorList = new List<Color>();
+            for (int i = x; i < x + pixelSize; i++)
+            {
+                for (int j = y; j < y + pixelSize; j++)
+                {
+                    colorList.Add(image.GetPixel(i, j));
+                }
+            }
+
+            // find the average of the colors
+            var averageColor = GetAverageColor(colorList);
+
+            // assign all the colors in block to the average color
+            for (int i = x; i < x + pixelSize; i++)
+            {
+                for (int j = y; j < y + pixelSize; j++)
+                {
+                    image.SetPixel(i, j, averageColor);
+                }
+            }
+        }
+
+        private Color GetAverageColor (List<Color> colorList)
+        {
+            List<int> redValueList = new List<int>();
+            List<int> blueValueList = new List<int>();
+            List<int> greenValueList = new List<int>();
+            foreach (Color color in colorList)
+            {
+                redValueList.Add(color.R);
+                blueValueList.Add(color.B);
+                greenValueList.Add(color.G);
+            }
+
+            return Color.FromArgb((int)redValueList.Average(), (int)greenValueList.Average(), (int)blueValueList.Average());
+        }
+
+        private void CheckPixelSize(string num)
+        {
+            int temp;
+            if (int.TryParse(num, out temp))
+            {
+                if (temp > 0)
+                {
+                    pixelSize = temp;
+                    CheckForCleanNumber(pixelSize);
+                }
+            }
+            else
+                UI_btnPixelate.Enabled = false;
+        }
+
+        private void CheckForCleanNumber(int num)
+        {
+            if (width % num == 0 && height % num == 0)
+                UI_btnPixelate.Enabled = true;
+            else
+                UI_btnPixelate.Enabled = false;
+
+        }
+
+        private void UI_txtPixelSize_TextChanged(object sender, EventArgs e)
+        {
+            CheckPixelSize(UI_txtPixelSize.Text);
+        }
+
+        private void RoundColors(Bitmap image)
+        {
+            ColorImageQuantizer ciq = new ColorImageQuantizer(new MedianCutQuantizer());
+            //Color[] colorTable = ciq.CalculatePalette((Bitmap)UI_PictureViewer.Image, 16);
+
+            UI_PictureViewer.Image = ciq.ReduceColors(image, colorReductionAmount);
+        }
+
+        private void UI_btnReduceColors_Click(object sender, EventArgs e)
+        {
+            RoundColors((Bitmap)UI_PictureViewer.Image);
+        }
+
+        private void UpdateColorReductionAmount(int num)
+        {
+            if (num > 2 && num < 256)
+            {
+                colorReductionAmount = num;
+                UI_btnReduceColors.Enabled = true;
+            }
+        }
+
+        private void UI_txtColorReduction_TextChanged(object sender, EventArgs e)
+        {
+            int temp;
+            if (int.TryParse(UI_txtColorReduction.Text, out temp))
+                UpdateColorReductionAmount(temp);
+        }
+    }
+}
